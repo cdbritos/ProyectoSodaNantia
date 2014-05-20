4e1231c55ab86272e29e70dfb6cd937b3910e914
cdbritos <cdbritos@gmail.com>
2014-05-19 23:30:48 -0300

    pelado mameta

--- ProyectoSodaNantia/src/org/uy/soda/nantia/MainActivity.java
+++ ProyectoSodaNantia/src/org/uy/soda/nantia/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-	
+		//PELADO MAMETAAAA
 	  @Override
 	  protected void onCreate(Bundle savedInstanceState) {
 	    super.onCreate(savedInstanceState);
